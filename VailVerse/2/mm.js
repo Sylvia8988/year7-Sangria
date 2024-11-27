85b55eda14f71352ddb8cb3187f1bb41b4ef57c6
冰冰 <Cherry228112011@gmail.com>
2024-11-27 21:05:17 +0630

    Delete VailVerse directory

--- VailVerse/2/mm.js
+++
@@ -1,8 +0,0 @@
-function showInfo(index) {
-    document.getElementById(`tooltip-${index}`).style.display = "block";
-  }
-  
-  function hideInfo(index) {
-    document.getElementById(`tooltip-${index}`).style.display = "none";
-  }
-  
